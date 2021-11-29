86e367f55bd94945404adf963bb3c6863e178557
chapei <anoopanthony@gmail.com>
2021-11-28 21:49:31 -0500

    StudentClient. To ask Tian for TODO help

--- Server/src/main/java/frontend/FE_Server.java
+++ Server/src/main/java/frontend/FE_Server.java
@@ -11,17 +11,17 @@ public class FE_Server {
         listener.start();
 
         String adminServicePath = "/AdminService";
-//        String studentServicePath = "/StudentService";
+        String studentServicePath = "/StudentService";
         String BASE_URI = "http://localhost:" + FRONTEND_PORT;
 
         AdminServiceImpl adminService = new AdminServiceImpl();
-//        StudentServiceImpl studentService = new StudentServiceImpl();
+        StudentServiceImpl studentService = new StudentServiceImpl();
 
         Endpoint.publish(BASE_URI + adminServicePath, adminService);
-//        Endpoint.publish(BASE_URI + studentServicePath, studentService);
+        Endpoint.publish(BASE_URI + studentServicePath, studentService);
 
         System.out.println("SOAP Service listening on " + BASE_URI + adminServicePath + "?wsdl");
-//        System.out.println("SOAP Service listening on " + BASE_URI + studentServicePath + "?wsdl");
+        System.out.println("SOAP Service listening on " + BASE_URI + studentServicePath + "?wsdl");
     }
 
 
---
+++ Server/src/main/java/frontend/StudentServiceImpl.java
@@ -0,0 +1,38 @@
+package frontend;
+
+import packet.Packet;
+import packet.parameter.*;
+
+import javax.jws.WebService;
+
+@WebService(name = "StudentService", endpointInterface = "frontend.StudentService")
+public class StudentServiceImpl implements StudentService {
+
+    @Override
+    public String bookRoom(String campusName, String roomNumber, String date, String timeSlot, String studentID) {
+        OperationParameter op = new BookRoomParameter(campusName, roomNumber ,date , timeSlot, studentID);
+        Packet request = new Packet(Operation.BOOK_ROOM, op, studentID.substring(0, 3));
+        return SFT_HA.make_SFT_HA(request);
+    }
+
+    @Override
+    public String getAvailableTimeSlot(String date) {
+        OperationParameter op = new GetAvailableTimeSlotParameter(date);
+        Packet request = new Packet(Operation.GET_AVAILABLE_TIME_SLOT, op, "DVLS3000");  // TODO: ask Tian
+        return SFT_HA.make_SFT_HA(request);
+    }
+
+    @Override
+    public String cancelBooking(String bookingID, String studentID) {
+        OperationParameter op = new CancelBookingParameter(bookingID, studentID);
+        Packet request = new Packet(Operation.CANCEL_BOOKING, op, "DVL");  // TODO: ask Tian
+        return SFT_HA.make_SFT_HA(request);
+    }
+
+    @Override
+    public String changeReservation(String bookingID, String newCampusName, String newRoomNo, String newTimeSlot, String studentID) {
+        OperationParameter op = new ChangeReservationParameter(bookingID, newCampusName, newRoomNo , newTimeSlot, studentID);
+        Packet request = new Packet(Operation.CHANGE_RESERVATION, op, studentID.substring(0, 3));
+        return SFT_HA.make_SFT_HA(request);
+    }
+}
